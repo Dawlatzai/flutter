0ee112f464856711c4078c4f20e5c1003b9d34f6
Dawlatzai <dawlatzai.ghousi@gmail.com>
2020-03-06 11:00:34 +0430

    Add button widget for adding project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,19 +6,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList Product'),
-        ),
-
-        body: Card(
-          child: Column(children: <Widget>[
-            Image.asset('assets/food.jpg'),
-            Text('Food Paradise')
-          ],
-          ),
-        ),
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('EasyList Product'),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text('Add Product'),
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text('Food Paradise')
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 }
