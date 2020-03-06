1c87cab5fa9bbb03b28fa788ed44ae05047c609d
Dawlatzai <dawlatzai.ghousi@gmail.com>
2020-03-06 12:09:14 +0430

    initstate from a statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList Product'),
       ),
-      body: ProductManager(),
+      body: ProductManager('Food Tester'),
     ));
   }
 }
